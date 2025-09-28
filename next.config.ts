5271c5866a43d1e90979b263a47bde80a5e42abf
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2025-09-28 13:44:02 +0530

    Add CORS headers to API routes in Next.js configuration
    - Implemented a new headers function in next.config.ts to add CORS headers for API routes.
    - Configured headers to allow credentials, specify allowed origins, and define permitted methods and headers for enhanced security and interoperability.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Be more specific in production
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "v0.dev" },
       { protocol: "https", hostname: "source.unsplash.com" },
-      { protocol: "https", hostname: "opkhandelwal.com" }, // Added to allow images from opkhandelwal.com
     ],
   },
 };
