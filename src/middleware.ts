d1090307461b1dc655e37490688a2d5b16eccdb3
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2025-10-08 00:24:17 +0530

    Remove runtime specification from middleware file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse, type NextRequest } from "next/server";
 import { auth } from "./auth";
 
-export const runtime = "nodejs";
 
 export async function middleware(request: NextRequest) {
   const session = await auth();
